44136d79c18a7d4bf4486d20189669c5e36e1e36
mirkotorrisi <mirko.torrisi92@gmail.com>
2024-09-22 09:19:15 +0200

    removed de rules, updated it rules, updated hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,15 @@
 import * as React from "react";
-import "./index.scss";
-import Navbar from "../components/navbar";
-import Footer from "../components/footer";
-import { NavProvider } from "../context/NavContext";
 import { Helmet } from "react-helmet";
-import chitarra from "../images/chitarra.svg";
-import sponsors from "../assets/sponsors";
-// import itaWordRules from "../assets/ita_gtorrisi_rules.docx";
-import itaPdfRules from "../assets/ita_gtorrisi_rules.pdf";
-import engPdfRules from "../assets/eng_gtorrisi_rules.pdf";
 import engPdfModel from "../assets/eng_gtorrisi_model.pdf";
+import engPdfRules from "../assets/eng_gtorrisi_rules.pdf";
 import itaPdfModel from "../assets/ita_gtorrisi_model.pdf";
-import dePdfModel from "../assets/de_gtorrisi_model.pdf";
-import dePdfRules from "../assets/de_gtorrisi_rules.pdf";
+import itaPdfRules from "../assets/ita_gtorrisi_rules.pdf";
+import sponsors from "../assets/sponsors";
+import Footer from "../components/footer";
+import Navbar from "../components/navbar";
+import { NavProvider } from "../context/NavContext";
+import chitarra from "../images/chitarra.svg";
+import "./index.scss";
 
 const IndexPage = () => {
   return (
@@ -38,34 +35,22 @@ const IndexPage = () => {
         <div className="flex justify-between px-5 pb-10 lg:px-48">
           <div className="home__hero lg:w-2/3" id="hero-section">
             <p className="my-5 text-left">
-              Seconda edizione del concorso musicale aperto ai Giovani
+              Terza edizione del concorso musicale aperto ai Giovani
               Chitarristi, Ensemble di Chitarre (dal duo in poi) e Musica da
               camera con chitarra, archi, fiati,legni o strumenti a pizzico in
               qualsiasi formazione.
             </p>
             <p>
-              Si svolge a Catania il 2 e il 3 Dicembre 2023 presso l’Auditorium
-              Sacro Cuore di via Milano - Catania.
+              Si svolge a Catania l'8 e il 9 Dicembre 2024 presso <a href="https://maps.app.goo.gl/GEauNDXJBoGTtHAa6" target="_blank">l’Auditorium Carlo Alberto Dalla Chiesa, Vico Mignemi, S. Gregorio (CT)</a>.
             </p>
             <div className="download_links mt-10">
               <h2 className="title mb-5">Scarica il regolamento:</h2>
-              {/* <a href={itaWordRules} download className="">
-                Regolamento italiano formato word
-              </a> */}
+
               <a href={itaPdfRules} download className="">
                 Regolamento italiano
               </a>
-              {/* <a href="../assets/eng_gtorrisi_rules.docx" download className="">
-                Rules in English word format
-              </a> */}
               <a href={engPdfRules} download className="">
                 Rules in English
-              </a>
-              {/* <a href="../assets/eng_gtorrisi_rules.docx" download className="">
-                Verordnung im deutschen Wortformat
-              </a> */}
-              <a href={dePdfRules} download className="">
-                Verordnung im deutschen
               </a>
             </div>
             <div className="download_links mt-10">
@@ -77,9 +62,6 @@ const IndexPage = () => {
               </a>
               <a href={engPdfModel} download className="">
                 English Application form
-              </a>
-              <a href={dePdfModel} download className="">
-                Bewerbungsformular in deutscher Sprache
               </a>
             </div>
             <div className="flex flex-col mt-20">
